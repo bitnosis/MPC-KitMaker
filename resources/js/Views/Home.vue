a8c3799bc140c0843be9c5374c1442274ec5cb25
Kylie V <analogpermutations@gmail.com>
2022-07-10 07:29:45 -0700

    Main ocmmit first commit

--- resources/js/Views/Home.vue
+++ resources/js/Views/Home.vue
@@ -8,8 +8,28 @@
                 <div class="p-4 bg-white">
                     
 
-                    You are Home !
+                  <div style="border-style:solid;" @dragover.prevent @drop.prevent>
+    <button @click="exportXPM">
+    Save Kit
+    </button>
 
+      <input type="text" v-model="Kit.name"/>
+      <input type="file" multiple @change="uploadFile"/>
+      <div @drop="dragFile" style="background-color:green;margin-bottom:10px;padding:10px;">
+        Or drag the file here  {{Kit.name}}
+        <div v-if="Files.length">
+          <div v-for="file in Files" :key="file">
+             {{file.name}}
+             <button @click="playAudio(file)">
+               Play
+             </button>
+             
+             <audio v-bind:id="'index-'+file.padIndex" class='sample_spots' hidden v-show="Files != ''"/>
+
+          </div>
+        </div>
+      </div>
+    </div>
 
                 </div>
             </div>
@@ -18,12 +38,50 @@
 </div>
 </template>
 
-
-
 <script>
+
 export default {
     data() {
         return {
+           Kit: {name:"This is the default kt Default"},
+    	   Files:[] 
+        }
+    },
+    methods: {
+        stopSounds() {
+            this.Files.forEach((value, index) => {
+                var sound = this.$el.querySelector('#index-'+value.padIndex);
+                sound.pause();
+                sound.currentTime = 0;
+                value.playing = false;
+            });
+        },
+        playAudio(file){
+            this.stopSounds();
+            var sound = this.$el.querySelector('#index-'+file.padIndex);
+            sound.play();
+            file.playing = true;
+        },
+        loadPreviews () {
+			this.Files.forEach((value, index) => {
+                let audio = this.$el.querySelector('#index-'+value.padIndex)
+                let reader = new FileReader();
+                reader.readAsDataURL( value.file );
+                reader.addEventListener('load', function(){
+                 audio.src = reader.result;
+                });
+            });
+        },
+        uploadFile(e) {
+            var that = this;
+            for(var i = 0; i<e.target.files.length; i++){
+               var d = {padIndex: i+1, name:e.target.files[i].name, playing:false, file: e.target.files[i]}
+       		   this.Files.push(d);
+            }
+            this.$forceUpdate();
+            setTimeout(function(){
+       	        that.loadPreviews();
+           }, 200)
         }
     }
 }
